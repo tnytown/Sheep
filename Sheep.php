6fa7e83c44d8a18e3f945701f6133f942fa6fd60
Andrew Pan <andrew.tianyi.pan@gmail.com>
2013-10-27 11:36:19 -0400

    Update

--- Sheep.php
+++ Sheep.php
@@ -1,3 +1,32 @@
 <?php
 
+/*
+__PocketMine Plugin__
+name=Sheep
+author=KnownUnown
+version=1.0
+apiversion=9,10,11
+class=Sheep
+*/
+
+class Sheep implements Plugin {
+    
+    private $api;
+    private $server;
+    private $config;
+    
+    public function __construct(ServerAPI $api, $server = false){
+        
+        $this->api = $api;
+        $this->server = ServerAPI::request();
+        
+        $this->config = new Config($this->api->plugin->configPath($this) . "config.yml", CONFIG_YAML, array(
+        "update-interval": 24,
+        "update-auto": true,
+        "api-url": "http://mcpedevs.pocketmine.net/api.php?ID=",
+        "plugins-installed": implode(", ", $this->api->plugin->getList()),
+        ));
+    }
+}
+
 ?>
