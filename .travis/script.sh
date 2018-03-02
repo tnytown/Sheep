c743a6d53cb1ff9cb994e0b17d2c996d3d7a67f6
KnownUnown <andrew.tianyi.pan@gmail.com>
2018-03-02 00:46:22 -0600

    build: cd into server directory before starting server

--- .travis/script.sh
+++ .travis/script.sh
@@ -2,6 +2,7 @@
 
 PHP=bin/php7/bin/php
 pm_dir=$TRAVIS_BUILD_DIR/../pm
+cd $pm_dir
 
 $PHP -dphar.readonly=0 $pm_dir/plugins/DevTools.phar --make $TRAVIS_BUILD_DIR --entry stub.php --out $pm_dir/plugins/Sheep.phar
 $PHP server.phar --no-wizard --disable-readline --debug.level=2 --pluginchecker.target=Sheep
