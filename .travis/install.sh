5d241d52ff7b45718012be71e004cef0a060f847
KnownUnown <andrew.tianyi.pan@gmail.com>
2018-03-02 01:25:02 -0600

    build: make jq return raw output, resilence cURL

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
-pm_url=$(curl -fsL "https://update.pmmp.io/api?channel=development" | jq ".download_url")
+pm_url=$(curl -fsL "https://update.pmmp.io/api?channel=development" | jq -r ".download_url")
 php_url="https://jenkins.pmmp.io/job/PHP-7.2-Linux-x86_64/lastSuccessfulBuild/artifact/PHP_Linux-x86_64.tar.gz"
-dt_url=$(curl -fsL "https://poggit.pmmp.io/releases.json?name=DevTools&latest-only" | jq ".[0].artifact_url")
+dt_url=$(curl -fsL "https://poggit.pmmp.io/releases.json?name=DevTools&latest-only" | jq -r ".[0].artifact_url")
 
+echo $pm_url
+echo $dt_url
 
 pm_dir=$TRAVIS_BUILD_DIR/../pm
 mkdir $pm_dir
 cd $pm_dir
 mkdir plugins/
 
-curl -fL $pm_url -o server.phar
-curl -fL $php_url -o php.tar.gz
-curl -fL $dt_url -o plugins/DevTools.phar
+curl -fsL $pm_url -o server.phar
+curl -fsL $php_url -o php.tar.gz
+curl -fsL $dt_url -o plugins/DevTools.phar
 
 tar -zxf php.tar.gz
 
--- .travis/script.sh
+++ .travis/script.sh
@@ -4,5 +4,5 @@ pm_dir=$TRAVIS_BUILD_DIR/../pm
 PHP=$pm_dir/bin/php7/bin/php
 cd $pm_dir
 
-$PHP -dphar.readonly=0 $pm_dir/plugins/DevTools.phar --make $TRAVIS_BUILD_DIR --entry stub.php --out $pm_dir/plugins/Sheep.phar
-$PHP $pm_dir/server.phar --no-wizard --disable-readline --debug.level=2 --pluginchecker.target=Sheep
+$PHP -dphar.readonly=0 plugins/DevTools.phar --make $TRAVIS_BUILD_DIR --entry stub.php --out plugins/Sheep.phar
+$PHP server.phar --no-wizard --disable-readline --debug.level=2 --pluginchecker.target=Sheep
