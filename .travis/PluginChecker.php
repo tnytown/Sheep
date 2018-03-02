ede303ba3af2c0f0807fd52855bbe7fa9ac5141a
KnownUnown <andrew.tianyi.pan@gmail.com>
2018-03-02 00:33:50 -0600

    Add the new .travis.yml file forgotten in the previous commit

--- .travis/PluginChecker.php
+++ .travis/PluginChecker.php
@@ -1,7 +1,23 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: apan
- * Date: 02/03/2018
- * Time: 00:02
+ * @name PluginChecker
+ * @author PEMapModder,KnownUnown
+ * @version 1.0.0
+ * @api 3.0.0-ALPHA11
+ * @main PluginChecker\PluginChecker
  */
+namespace PluginChecker;
+class PluginChecker extends \pocketmine\plugin\PluginBase {
+	public function onEnable() {
+		$this->getServer()->getScheduler()->scheduleDelayedTask(new CheckTask($this), 1);
+	}
+}
+class CheckTask extends \pocketmine\scheduler\PluginTask {
+	public function onRun(int $t){
+		$plugin = $this->owner->getServer()->getPluginManager()->getPlugin(
+			$this->owner->getServer()->getProperty("pluginchecker.target"));
+
+		if($plugin !== null && $plugin->isEnabled()) $this->owner->getServer()->shutdown();
+		else exit(1);
+	}
+}
--- .travis/script.sh
+++ .travis/script.sh
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+
+PHP=bin/php7/bin/php
+
+$PHP -dphar.readonly=0 plugins/DevTools.phar --make $TRAVIS_BUILD_DIR --out plugins/Sheep_$(echo $TRAVIS_COMMIT | cut -c 1-6 -).phar
+$PHP server.phar --no-wizard --disable-readline --debug.level=2 --pluginchecker.target=Sheep
